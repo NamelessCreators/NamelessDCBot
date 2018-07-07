7c30e2eb2ec62af9e76112d684e581a785eb1abb
NamelessCreators <40916431+NamelessCreators@users.noreply.github.com>
2018-07-07 15:24:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,4 +12,4 @@ async def on_ready():
     print ("The NamelessCreators bot is ready")
     print ("Im running on" + bot.user.name)
 
-    bot.run("NDY1MDk5MDc1OTk0MjU1MzYw.DiJInw.hRIou8MBxLg3k6C0ClLTwvVRUCw")
+    bot.run("TOKEN HIER")
